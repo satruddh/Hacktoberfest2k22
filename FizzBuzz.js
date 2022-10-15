32ab58f3b6db4c26d0f086ef934aa6577620162a
Khushi Jha <94845508+Khushi260@users.noreply.github.com>
2022-10-15 23:05:25 +0530

    Create FizzBuzz.js

---
+++ FizzBuzz.js
@@ -0,0 +1,31 @@
+var output = [];
+var count =1;
+
+function FizzBuzz(){
+     
+     while(count<=100){
+          
+          if (count % 5===0 && count%3===0){
+        output.push("FizzBuzz");
+    }
+      else if (count % 5===0){
+        output.push("Buzz");
+    }
+
+    else if (count % 3===0){
+        output.push("Fizz");
+    }
+        
+    else{
+         output.push(count);
+    }
+   
+    count++;
+     }
+     
+    
+
+   console.log(output);
+}
+
+FizzBuzz();
